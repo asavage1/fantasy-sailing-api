dc110602f46e51523a28518f252583584e607099
Andrew Savage <andrew.savage.270@gmail.com>
2019-02-16 21:33:44 -0500

    Feature/updated travis (#5)
    * Added gitignore
    
    * Updated travis scripts

---
+++ tests/ex_test.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+
+// if (process.env.NODE_ENV != "test") {
+// 	process.exit(1);
+// }
+
+describe("GET /", function () {
+	describe("balh", function () {
+		it("should pass", function () {
+			assert.equal(true, true);
+		});
+	});
+});
